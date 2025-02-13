34f2dfa0239528bb2fc7cfeb3f501795d6900d4f
solji <solji.choi89@gmail.com>
2025-02-13 15:15:27 +0900

    list

--- src/main/kotlin/JMain.java
+++ src/main/kotlin/JMain.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class JMain {
     public static void main(String[] args) {
-        Person person = new Person("Alice");
-        person.greet();
+        List<String> names = new ArrayList<>();
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Charlie");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,10 @@
 package com.ll
 
-class Person(val name: String) {
-    fun greet() {
-        println("Hello, my name is $name")
-    }
-}
-
 fun main() {
-    val person = Person("Alice")
-    person.greet()
+    val names = listOf("Alice", "Bob", "Charlie") // 불변 리스트
+    val names2 = mutableListOf("dlf", "dl", "tka") //추가 가능한 리스트
+
+    for (name in names) {
+        println(name)
+    }
 }
