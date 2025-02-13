dda1eb2403f84fca9c1069e22bfcc577609ff220
solji <solji.choi89@gmail.com>
2025-02-13 14:49:01 +0900

    funtion

--- src/main/kotlin/JMain.java
+++ src/main/kotlin/JMain.java
@@ -1,7 +1,10 @@
 public class JMain {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Count: " + i);
-        }
+        int result = add(5, 10);
+        System.out.println("Result: " + result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 package com.ll
 
 fun main() {
-    for (i in 0 until 5) { // 0부터 4까지 반복
-        println("Count: $i")
-    }
+    val result = add(5, 10)
+    println("Result: $result")
+}
+
+fun add(a: Int, b: Int): Int {
+    return a + b
 }
