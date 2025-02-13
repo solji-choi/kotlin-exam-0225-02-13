fbfe8bb3478709334e92899779579b00bb04e9cc
solji <solji.choi89@gmail.com>
2025-02-13 15:15:55 +0900

    map

--- src/main/kotlin/JMain.java
+++ src/main/kotlin/JMain.java
@@ -1,15 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JMain {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-        names.add("Alice");
-        names.add("Bob");
-        names.add("Charlie");
+        Map<String, Integer> ages = new HashMap<>();
+        ages.put("Alice", 25);
+        ages.put("Bob", 30);
+        ages.put("Charlie", 35);
 
-        for (String name : names) {
-            System.out.println(name);
+        for (Map.Entry<String, Integer> entry : ages.entrySet()) {
+            System.out.println(entry.getKey() + " is " + entry.getValue() + " years old.");
         }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
 package com.ll
 
 fun main() {
-    val names = listOf("Alice", "Bob", "Charlie") // 불변 리스트
-    val names2 = mutableListOf("dlf", "dl", "tka") //추가 가능한 리스트
+    val ages = mapOf("Alice" to 25, "Bob" to 30, "Charlie" to 35) // 불변 맵
 
-    for (name in names) {
-        println(name)
+    for ((name, age) in ages) {
+        println("$name is $age years old.")
     }
 }
