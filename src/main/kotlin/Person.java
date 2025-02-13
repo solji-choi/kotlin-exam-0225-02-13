9d17bde6d1d1115a3f56de5f643dadd3587549ba
solji <solji.choi89@gmail.com>
2025-02-13 15:39:27 +0900

    data class

--- src/main/kotlin/Person.java
+++ src/main/kotlin/Person.java
@@ -1,12 +1,37 @@
-public class Person {
-    String name;
+import java.util.Objects;
 
-    // 생성자
-    public Person(String name) {
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
-    public void greet() {
-        System.out.println("Hello, my name is " + name);
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
     }
 }
