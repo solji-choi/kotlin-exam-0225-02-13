d556159871bcc6e70aae347118be8281ba02f751
solji <solji.choi89@gmail.com>
2025-02-13 14:25:35 +0900

    basic1

---
+++ src/main/kotlin/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    int age;
+    String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+}
