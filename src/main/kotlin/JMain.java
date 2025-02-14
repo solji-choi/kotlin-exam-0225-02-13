d9c34917ce97f805ddac641bcaf3d3e8b405f455
solji <solji.choi89@gmail.com>
2025-02-14 10:15:58 +0900

    let

--- src/main/kotlin/JMain.java
+++ src/main/kotlin/JMain.java
@@ -1,10 +1,10 @@
 public class JMain {
     public static void main(String[] args) {
-        Object obj = "Hello";
+        String name = null;
 
-        if (obj instanceof String) {
-            String str = (String) obj; // 명시적 캐스팅 필요
-            System.out.println(str.length());
+        if (name != null) {
+            int length = name.length();
+            System.out.println("Length: " + length);
         }
     }
 }
