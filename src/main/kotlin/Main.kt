9c0c31061b3c5d211791ccf4faae34c7d9821428
solji <solji.choi89@gmail.com>
2025-02-14 10:51:35 +0900

    with

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,30 @@
 package com.ll
 
-fun main() {
-    val result = run {
-        val x = 5
-        x * 2 + 10
+class Person(val name: String, var age: Int, var weight: Double, var salary: Double) {
+    fun increaseAge(years: Int) {
+        age += years
     }
 
-    println("Result: $result")
+    fun increaseWeight(kg: Double) {
+        weight += kg
+    }
+
+    fun increaseSalary(amount: Double) {
+        salary += amount
+    }
+
+    fun getInfo() = "$name is $age years old, weighs $weight kg, and earns $$salary"
+}
+
+fun main() {
+    val person = Person("Alice", 30, 55.0, 50000.0)
+
+    val info = with(person) {
+        increaseAge(5)
+        increaseWeight(3.5)
+        increaseSalary(10000.0)
+        getInfo() // 마지막 실행된 값이 반환됨
+    }
+
+    println(info)
 }
--- src/main/kotlin/Person.java
+++ src/main/kotlin/Person.java
@@ -1,25 +1,41 @@
 class Person {
     String name;
     int age;
+    double weight;
+    double salary;
 
-    public Person setName(String name) {
+    public Person(String name, int age, double weight, double salary) {
         this.name = name;
-        return this;
-    }
-
-    public Person setAge(int age) {
         this.age = age;
-        return this;
+        this.weight = weight;
+        this.salary = salary;
     }
 
-    public void printInfo() {
-        System.out.println(name + " is " + age + " years old.");
+    public void increaseAge(int years) {
+        this.age += years;
+    }
+
+    public void increaseWeight(double kg) {
+        this.weight += kg;
+    }
+
+    public void increaseSalary(double amount) {
+        this.salary += amount;
+    }
+
+    public String getInfo() {
+        return name + " is " + age + " years old, weighs " + weight + "kg, and earns $" + salary;
     }
 
     public static void main(String[] args) {
-        Person person = new Person()
-                .setName("Alice")
-                .setAge(30);
-        person.printInfo();
+        Person person = new Person("Alice", 30, 55.0, 50000.0);
+
+        // with와 비슷한 방식으로 메서드 체이닝 사용
+        person.increaseAge(5);
+        person.increaseWeight(3.5);
+        person.increaseSalary(10000);
+        String info = person.getInfo();
+
+        System.out.println(info);
     }
 }
