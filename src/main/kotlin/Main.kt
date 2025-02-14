6a3e65467b9562c4a07cb6bad819c3db8ce7d0d3
solji <solji.choi89@gmail.com>
2025-02-14 10:22:57 +0900

    let2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,13 @@
 package com.ll
 
 fun main() {
-    val name: String? = null
+    val name: String? = "aaa"
 
-    name?.let {
+    val age = name?.let {
         println("Length: ${it.length}")
+
+        10
     }
+
+    println(age) //let은 가장 마지막 값이 리턴됨
 }
