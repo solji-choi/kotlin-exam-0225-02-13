cc29443750f82b6e93c264108aff7ca7ba71a881
solji <solji.choi89@gmail.com>
2025-02-14 09:24:35 +0900

    String Template

--- src/main/kotlin/JMain.java
+++ src/main/kotlin/JMain.java
@@ -1,6 +1,9 @@
 
 public class JMain {
     public static void main(String[] args) {
-        //System.out.println(StringUtils.greet("Alice"));
+        String name = "Alice";
+        int age = 25;
+
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
     }
 }
